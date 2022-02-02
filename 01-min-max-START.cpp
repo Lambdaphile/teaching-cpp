2d5d930363ab86fd14601772e3e1510ad343b658
λ • genesis <rwxgenesis@gmail.com>
2022-02-02 23:27:38 +0300

    Create 01-min-max-START.cpp

---
+++ 01-min-max-START.cpp
@@ -0,0 +1,32 @@
+/**
+ * Exercise:
+ *
+ *   Write a program that reads from the console a series of
+ *   integers and prints the smallest and largest of them.
+ *
+ * Solution & Guidelines:
+ *
+ *   First, declare and define the following variables:
+ *     int n, sequence[100],
+ *         currNum, minNum = INT_MAX, maxNum = INT_MIN;
+ *
+ *   And then:
+ *     1. Read the count of numbers user wants to enter (int n).
+ *     2. Consequently read the numbers and keep them in
+ *        an one dimensional array (int sequence[100]).
+ *     3. Iterate over the array and do the following operations:
+ *        - if current number < min number then min number = current number
+ *        - if current number > max number then max number = current number
+ *     4. Print min and max numbers.
+ *
+ *   Please make sure the code is clean and readable.
+ */
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // You can do it 💪
+}
